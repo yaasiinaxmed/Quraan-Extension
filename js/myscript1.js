5837ded126fd0d1ffbf49a56d80bfb25d31aa7da
yaasiinaxmed <yaasiinartsyaasiinarts@gmail.com>
2022-11-11 16:09:15 +0300

    initial commit

--- js/myscript1.js
+++ js/myscript1.js
@@ -2,7 +2,7 @@ window.onload = function() {
     const createDate = {
         url: "background.html",
         type: "popup",
-        width: 1200,
+        width: 1300,
         height: 700,
     };
     chrome.windows.create(createDate, () => {});
